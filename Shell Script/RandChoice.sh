2a6e4e56c3aab79e7665b289216187d069b652dc
ZVitoX <ZVitoX01@gmail.com>
2024-06-07 15:36:12 +0300

    Random Choice Completed

--- Shell Script/RandChoice.sh
+++ Shell Script/RandChoice.sh
@@ -2,13 +2,14 @@
 # Author: ZVitoX (GitHub.com/ZVitoX)
 # Script for selecting a random option from a list of provided items
 
-bold_red="\e[1;31m"
-bold_white="\e[1;37m"
-bold_blue="\e[1;36m"
+bold_red="\e[1;31m" 
+bold_blue="\e[1;36m" 
+bold_green="\e[1;32m"
+bold_white="\e[1;37m" 
 
 # Function to randomly choose an item from the provided arguments
 random_choice () {
-    # Perform the random selection multiple times to enhance randomness
+	# Perform the random selection multiple times to enhance randomness
 	for _ in {1..6}
 	do
 		choice=$(shuf -n1 -e "$@")	
@@ -20,7 +21,7 @@ random_choice () {
 # Check if at least one argument is provided
 if [ "$#" -lt 2 ]
 then
-	echo -e "\n${bold_red}Usage: ${bold_white}$0 ${bold_blue}Item1 Item2 ...\n"
+	echo -e "\n${bold_red}Usage: ${bold_white}$0 ${bold_green}Item1 Item2 ...\n"
 	exit 1
 
 else 
